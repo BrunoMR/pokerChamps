c4f6d6b866436f06a60fc1e14b70f196dcba34e3
Bruno Machado <brunomachadorodrigues@gmail.com>
2023-06-17 00:33:58 -0300

    delete useless service

--- PokerChamps/Poker.Domain/Services/Config/Interfaces/IConfiCreateService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Poker.Domain.Entities.Config;
-using Poker.Domain.Services.Shared.Interfaces;
-
-namespace Poker.Domain.Services.Config.Interfaces;
-
-// public interface IConfiCreateService : ICreateService<Configs>
-// {
-//     
-// }
